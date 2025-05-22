93c0c84cff0d3a4a43c42a62105be21bf6fd7f7c
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2025-05-23 01:19:42 +0600

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -3,16 +3,33 @@ Shery.makeMagnet(".magnet");
 Shery.hoverWithMediaCircle(".hvr", {videos: ["./0.mp4", "./2.mp4", "./3.mp4"],});
 
 
-gsap.to("", {
-    scrolltrigger:{
-        trigger: ".fimages",
+gsap.to(".fleftelem", {
+  scrollTrigger: {
+    trigger: "#fimages",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".last",
+    scrub: 1,
+  },
+  y: "-300%",
+  ease: Power1,
+});
+
+let sections = document.querySelectorAll(".fleftelem");
+Shery.imageEffect(".imgs", {
+  style: 4,
+  config: { onMouse: { value: 1 } },
+  slideStyle: (setScroll) => {
+    sections.forEach(function (section, index) {
+      ScrollTrigger.create({
+        trigger: section,
         start: "top top",
-        end: "bottom bottom",
-       endtrigger: ".last",
-       scrub: 1,
-
-
-    },
-    y:"-300%",
-    ease: Power1
-})
+        scrub: 1,
+        onUpdate: function (prog) {
+          setScroll(prog.progress + index);
+        },
+      });
+    });
+  },
+});
